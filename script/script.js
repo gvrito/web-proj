c3862c0608ff000eb2bbb1ee0333440c55ea514b
Gvrito123 <gvritagio@gmail.com>
2020-12-18 02:15:43 +0400

    added uppercase funct/added city validation/bugfixes

--- script/script.js
+++ script/script.js
@@ -22,22 +22,6 @@ let cityNamesArray = ['Tbilisi','Batumi','Kutaisi','Telavi','Mestia','Bakuriani'
 let euroCities = ['London','Berlin','Manchester','Paris','Baku','Minsk','Prague','Riga','Monaco','Copenhagen','Lisbon','Moscow','Bucharest','Helsinki','Tbilisi','Belgrade','Madrid','Athens','Budapest','Bern','Ankara','Kiev','Rome'];
 let euroCitiesText = document.getElementById('eurocities');
 
-const upperCaseFirst = (string) => {
-    let preserve = string[0];
-    string = string.slice(1,string.length);
-    preserve = preserve.toUpperCase();
-    preserve += string;
-    return preserve;
-}
-
-const upperCaseFirstAll = (string) => {
-    let strArray = string.split(' ');
-    for(let i = 0; i < strArray.length; i++){
-        strArray[i] = upperCaseFirst(strArray[i])
-    }
-    return strArray.join(' ');
-}
-
 const activateModal = () => {
     modal.style.display = 'flex';
     modal.style.backgroundColor = 'rgba(0,0,0,0.4)';
@@ -57,6 +41,19 @@ const getMonthName = (number) => {
     const arr = ['January','February','March','April','May','June','July','August','September','October','November','December']
     return arr[number];
 }
+const upperCase = (string) => {
+    let splittedStr = string.toLowerCase().split(" ");
+    let ansArr = [];
+    for(let i in splittedStr) {
+        let testStr = splittedStr[i];
+        let preserve = testStr[0];
+        let sub = testStr.substring(1,testStr.length);
+        let ans = preserve.toUpperCase() + sub;
+        ansArr.push(ans);
+    }
+    ans = ansArr.join(" ");
+    return ans;
+}
 searchBar.addEventListener('click',function() {
     header.style.backgroundColor = 'rgb(220,220,220)'
 })
@@ -66,15 +63,48 @@ container.addEventListener('click', function() {
 })
 
 const getCityWeather = function(input) {
-    activateModal();
     modalAlert.style.backgroundImage = 'url'+'('+`media/default.png`+')';
     let lowerCity = input.toLowerCase();
+    //am api-t potoebs vigeb rom mere background image-d davakeno modal popupze
+    fetch(`https://api.teleport.org/api/urban_areas/slug:${lowerCity}/images/`)
+    .then(responsee => {
+        let data = responsee.json()
+        .then(data => {
+            photo = data.photos[0].image.web
+            modalAlert.style.backgroundPositionY = '0px'
+            modalAlert.style.backgroundImage = 'url'+'('+`${photo}`+')';
+            //es switchi imitom makvs rom searchshi tu am 6dan romelime kalaki chaceres gansxvavebul background images akenebda(main pageze rac mak imisgan gansxvavebuls)
+            switch(lowerCity) {
+                case 'tbilisi':
+                    modalAlert.style.backgroundImage = 'url'+'('+`media/${0}.png`+')';
+                    modalAlert.style.backgroundPositionY = '-100px'
+                    break;
+                case 'batumi':
+                    modalAlert.style.backgroundImage = 'url'+'('+`media/${1}.png`+')';
+                    break;
+                case 'kutaisi':
+                    modalAlert.style.backgroundImage = 'url'+'('+`media/${2}.png`+')'
+                    break;
+                case 'telavi':
+                    modalAlert.style.backgroundImage = 'url'+'('+`media/${3}.png`+')'
+                    break;
+                case 'mestia':
+                    modalAlert.style.backgroundImage = 'url'+'('+`media/${4}.png`+')'
+                    break;
+                case 'bakuriani':
+                    modalAlert.style.backgroundImage = 'url'+'('+`media/${5}.png`+')'
+                    break;
+            }
+        })
+    })
+     //switchi kvemot meorejer imito makvs rom zeda api requesti tu damirejecta main pageze arsebul 6 cardis modals mainc kondes background img
     switch(lowerCity) {
         case 'tbilisi':
-            modalAlert.style.backgroundImage = 'url'+'('+`media/${0}.png`+')'
+            modalAlert.style.backgroundImage = 'url'+'('+`media/${0}.png`+')';
+            modalAlert.style.backgroundPositionY = '-100px'
             break;
         case 'batumi':
-            modalAlert.style.backgroundImage = 'url'+'('+`media/${1}.png`+')'
+            modalAlert.style.backgroundImage = 'url'+'('+`media/${1}.png`+')';
             break;
         case 'kutaisi':
             modalAlert.style.backgroundImage = 'url'+'('+`media/${2}.png`+')'
@@ -89,27 +119,76 @@ const getCityWeather = function(input) {
             modalAlert.style.backgroundImage = 'url'+'('+`media/${5}.png`+')'
             break;
     }
-    fetch(`https://api.teleport.org/api/urban_areas/slug:${lowerCity}/images/`)
-    .then(responsee => responsee.json())
-    .then(data => {
-        photo = data.photos[0].image.web
-        modalAlert.style.backgroundImage = 'url'+'('+`${photo}`+')';
+    //kveda fetchit vigeb amindis prognozs da modal popup-ze gamomakvs
+    fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${input}&units=metric&cnt=7&appid=${API}`)
+    .then(response => {
+        let data = response.json()
+        .then(data => {
+            activateModal();
+            searchBar.style.outline = '0px'
+            modalCityName.innerHTML = input;
+            for(let i = 6; i<13; i++){
+                //am date-ebs ar miakcio kuradgeba 3 dge davxarje ro gamerkvia ro turme javascriptshi 0-11 aris tveebi da ara 1-12 da magas vhandlav
+                let realDate = new Date();
+                let day = new Date(realDate.getFullYear(),realDate.getMonth(),realDate.getDate()+(i-6));
+                let displayDate = day.getFullYear() + " " + getMonthName(day.getMonth()) + " " + day.getDate();
+                date[i-6].innerHTML = displayDate;
+                weather[i].innerHTML = upperCase(data.list[i-6].weather[0].description);
+                temperatures[i].innerHTML = Math.floor(data.list[i-6].temp.day) + "°";
+                max[i].innerHTML = "Max Temperature: " + Math.floor(data.list[i-6].temp.max) + "°";
+                min[i].innerHTML = "Min Temperature: " + Math.floor(data.list[i-6].temp.min) + "°";
+                switch(data.list[i-6].weather[0].icon) {
+                    case '01d': 
+                        weatherIcon[i].src = 'media/01d.png'; 
+                        break;
+                    case '02d':
+                        weatherIcon[i].src = 'media/02d.png'
+                        break;
+                    case '03d':
+                        weatherIcon[i].src = 'media/03d.png'
+                        break;
+                    case '04d':
+                        weatherIcon[i].src = 'media/04d.png'
+                        break;
+                    case '09d':
+                        weatherIcon[i].src = 'media/09d.png'
+                        break;
+                    case '10d':
+                        weatherIcon[i].src = 'media/10d.png'
+                        break;
+                    case '11d':
+                        weatherIcon[i].src = 'media/11d.png'
+                        break;
+                    case '13d':
+                        weatherIcon[i].src = 'media/13d.png'
+                        break;
+                    case '50':
+                        weatherIcon[i].src = 'media/50d.png'
+                        break;
+                    default: weatherIcon[i].src = 'media/01d.png'
+                }
+            }
+        })//am catchit cota ucnaurad vhandlav rejects magram mushaobs amitom kitxvebs ar vsvav
+        .catch(error => {
+            closeModal()//modali mainc ixsneboda erroris miuxedavad da closes funkcia davamate azrze arvar raxdeba da verc vigeb
+            searchBar.style.outline = '1px solid red'
+            alert('City with that name was not found');
+        }) 
     })
-    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${input}&units=metric&cnt=7&appid=${API}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        modalCityName.innerHTML = input;
-        for(let i = 6; i<13; i++){
-            let realDate = new Date();
-            let day = new Date(realDate.getFullYear(),realDate.getMonth(),realDate.getDate()+(i-6));
-            let displayDate = day.getFullYear() + " " + getMonthName(day.getMonth()) + " " + day.getDate();
-            date[i-6].innerHTML = displayDate;
-            weather[i].innerHTML = upperCaseFirstAll(data.list[i-6].weather[0].description);
-            temperatures[i].innerHTML = Math.floor(data.list[i-6].temp.day) + "°";
-            max[i].innerHTML = "Max Temperature: " + Math.floor(data.list[i-6].temp.max) + "°";
-            min[i].innerHTML = "Min Temperature: " + Math.floor(data.list[i-6].temp.min) + "°";
-            switch(data.list[i-6].weather[0].icon) {
+}
+
+
+for(let i = 0; i < 6; i++) { //amit main pageze 6 cards vadzlev aminds(vici cudad gavakete mara tavidanve ese vkeni da exla mezareba shecvla)
+    citynames[i].innerHTML = cityNamesArray[i];
+    fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityNamesArray[i]}&units=metric&cnt=7&appid=${API}`)
+    .then(response => {
+        let data = response.json()
+        .then(data => {
+            weather[i].innerHTML = upperCase(data.list[0].weather[0].description);
+            temperatures[i].innerHTML = Math.floor(data.list[0].temp.day) + "°";
+            max[i].innerHTML = "Max Temperature: " + Math.floor(data.list[0].temp.max) + "°";
+            min[i].innerHTML = "Min Temperature: " + Math.floor(data.list[0].temp.min) + "°";
+            switch(data.list[0].weather[0].icon) {
                 case '01d': 
                     weatherIcon[i].src = 'media/01d.png'; 
                     break;
@@ -139,57 +218,14 @@ const getCityWeather = function(input) {
                     break;
                 default: weatherIcon[i].src = 'media/01d.png'
             }
-        }
+            cityCards[i].style.backgroundImage = 'url'+'('+`media/${i}.png`+')';
+        })
     })
-}
-
-
-for(let i = 0; i < 6; i++) {
-    citynames[i].innerHTML = cityNamesArray[i];
-    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityNamesArray[i]}&units=metric&cnt=7&appid=${API}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        weather[i].innerHTML = upperCaseFirstAll(data.list[0].weather[0].description);
-        temperatures[i].innerHTML = Math.floor(data.list[0].temp.day) + "°";
-        max[i].innerHTML = "Max Temperature: " + Math.floor(data.list[0].temp.max) + "°";
-        min[i].innerHTML = "Min Temperature: " + Math.floor(data.list[0].temp.min) + "°";
-        switch(data.list[0].weather[0].icon) {
-            case '01d': 
-                weatherIcon[i].src = 'media/01d.png'; 
-                break;
-            case '02d':
-                weatherIcon[i].src = 'media/02d.png'
-                break;
-            case '03d':
-                weatherIcon[i].src = 'media/03d.png'
-                break;
-            case '04d':
-                weatherIcon[i].src = 'media/04d.png'
-                break;
-            case '09d':
-                weatherIcon[i].src = 'media/09d.png'
-                break;
-            case '10d':
-                weatherIcon[i].src = 'media/10d.png'
-                break;
-            case '11d':
-                weatherIcon[i].src = 'media/11d.png'
-                break;
-            case '13d':
-                weatherIcon[i].src = 'media/13d.png'
-                break;
-            case '50':
-                weatherIcon[i].src = 'media/50d.png'
-                break;
-            default: weatherIcon[i].src = 'media/01d.png'
-        }
-        cityCards[i].style.backgroundImage = 'url'+'('+`media/${i}.png`+')';
-    })
+    
 }
 
 closeBtn.addEventListener('click', closeModal)
-
+//am kveda kodit main pageze 6 cards vadzlev background imagebs
 for(let i = 0; i < cityCards.length; i++) {
     cityCards[i].addEventListener('click',() => {
         getCityWeather(cityNamesArray[i]);
@@ -197,14 +233,17 @@ for(let i = 0; i < cityCards.length; i++) {
     } )
 }
 
-
+//am kveda kodit vaketeb morbnenal strikons
 let euroCitiesTextArray = []
 
 for(let i = 0; i < euroCities.length; i++) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${euroCities[i]}&units=metric&cnt=7&appid=${API}`)
-    .then(response => response.json())
-    .then(data =>{
-        let temp = Math.floor(data.list[0].temp.day);
-        euroCitiesText.innerHTML += `| ${euroCities[i]}: ${temp}° |`;
+    fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${euroCities[i]}&units=metric&cnt=7&appid=${API}`)
+    .then(response => {
+        let data = response.json()
+        .then(data =>{
+            let temp = Math.floor(data.list[0].temp.day);
+            euroCitiesText.innerHTML += `| ${euroCities[i]}: ${temp}° |`;
+        })
     })
+    
 }
